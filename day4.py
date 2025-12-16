c1ebac072d543fa83d78b00a2393f947fe4fca5b
lotusfish <unown256@gmail.com>
2025-12-16 23:31:34 +0000

    [D4P2] Removed debug for solved functions

--- day4.py
+++ day4.py
@@ -9,10 +9,9 @@ def inputToLists(filename: str) -> list[str]:
 
 #takes in the data as well as an x and y co-ordinate and returns the number of
 #neighbouring rolls
-def getRollNeighbours(data: list[str], x:int, y:int, debug=False) -> int:
+def getRollNeighbours(data: list[str], x:int, y:int) -> int:
 	neighbours = 0
 	DIR_VEC = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
-	debugOut = ""
 	for vec in DIR_VEC:
 		try: #allows out-of-bounds to be handled and treated as a lack of roll rather than error
 			if y + vec[0] < 0 or x + vec[1] < 0:
@@ -21,25 +20,19 @@ def getRollNeighbours(data: list[str], x:int, y:int, debug=False) -> int:
 			toCheck = data[y + vec[0]][x + vec[1]]
 			if toCheck == "@":
 				neighbours += 1
-			debugOut += toCheck
 		except IndexError:
-			debugOut += "*"
 			pass 
-	if debug:
-		print(f"{neighbours} neighbours at {x}, {y}. {debugOut}")
 	return neighbours
 
 #takes in the puzzle, returns a solution
-def solve(data: list[str], threshold=4, debug=False) -> int:
+def solve(data: list[str], threshold=4) -> int:
 	accessibleRolls = 0
 	foundAt = []
 	for y in range(len(data)):
 		for x in range(len(data[y])):
-			if getRollNeighbours(data, x, y, debug) < threshold and data[y][x] == "@":
+			if getRollNeighbours(data, x, y) < threshold and data[y][x] == "@":
 				accessibleRolls += 1
 				foundAt.append((y, x))
-	if debug == True:
-		print(foundAt)
 	return accessibleRolls
 
 testData = {
