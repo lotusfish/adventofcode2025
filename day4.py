a5c3f3126d256d2d5573aa68415b22d1d97cd38f
lotusfish <unown256@gmail.com>
2025-12-16 23:27:50 +0000

    [D4P1] Solve

--- day4.py
+++ day4.py
@@ -12,6 +12,7 @@ def inputToLists(filename: str) -> list[str]:
 def getRollNeighbours(data: list[str], x:int, y:int, debug=False) -> int:
 	neighbours = 0
 	DIR_VEC = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+	debugOut = ""
 	for vec in DIR_VEC:
 		try: #allows out-of-bounds to be handled and treated as a lack of roll rather than error
 			if y + vec[0] < 0 or x + vec[1] < 0:
@@ -20,17 +21,34 @@ def getRollNeighbours(data: list[str], x:int, y:int, debug=False) -> int:
 			toCheck = data[y + vec[0]][x + vec[1]]
 			if toCheck == "@":
 				neighbours += 1
-			if debug:
-				print(toCheck)
+			debugOut += toCheck
 		except IndexError:
-			if debug:
-				print("*")
+			debugOut += "*"
 			pass 
+	if debug:
+		print(f"{neighbours} neighbours at {x}, {y}. {debugOut}")
 	return neighbours
+
+#takes in the puzzle, returns a solution
+def solve(data: list[str], threshold=4, debug=False) -> int:
+	accessibleRolls = 0
+	foundAt = []
+	for y in range(len(data)):
+		for x in range(len(data[y])):
+			if getRollNeighbours(data, x, y, debug) < threshold and data[y][x] == "@":
+				accessibleRolls += 1
+				foundAt.append((y, x))
+	if debug == True:
+		print(foundAt)
+	return accessibleRolls
 
 testData = {
 	"letters"  : ["abc","def","ghi"],
 	"allRolls" : ["@@@","@@@","@@@"],
-	"allBlank" : ["...","...","..."]
+	"allBlank" : ["...","...","..."],
+	"site"     : ["..@@.@@@@.", "@@@.@.@.@@", "@@@@@.@.@@", "@.@@@@..@.", "@@.@@@@.@@", ".@@@@@@@.@", ".@.@.@.@@@", ".@@@@@@@@.", "@.@.@@@.@."]
 }
 
+puzzleInput = inputToLists("day4input.txt")
+
+print(solve(puzzleInput))
